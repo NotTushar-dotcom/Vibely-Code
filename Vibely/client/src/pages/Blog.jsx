3327c037222990828d36691fd3cca437821ae874
Tushar <tushh45@gmail.com>
2025-11-09 22:46:44 +0530

    Y

--- Vibely/client/src/pages/Blog.jsx
+++ Vibely/client/src/pages/Blog.jsx
@@ -41,7 +41,10 @@ function Blog() {
         <p className="text-primary py-4 font-medium">Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
         <h1 className="text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800">{data.title}</h1>
         <h2 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h2>
-        <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary">By Vibelycode</p>
+        <div className="inline-flex items-center justify-center gap-2 px-6 py-1.5 mb-6 border border-primary/40 bg-primary/10 rounded-full text-sm text-primary">
+          <p>By Vibelycode</p>
+          <img src={assets.star_icon} className='w-2.5' alt="" />
+        </div>
       </div>
 
 
